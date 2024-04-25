f35b24492fe8b4e29ca4d4a4c6ce89db7c06376b
X3ne <X3ne@users.noreply.github.com>
2024-04-25 16:19:41 +0200

    feat: add VERSION env check for python build script & .env.example

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+import os
+
+version = os.getenv('VERSION')
 
 setup(
     name='grpc-user-management-auth',
-    version='0.1.0rc1',
-    # packages=find_packages(),
+    version=version,
     package_dir={'grpc_user_management_auth': 'gen/src/python/auth'},
     install_requires=[
         'grpcio',
