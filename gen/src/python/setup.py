5cdc667ee95adecdcd431b185a39531ac0efa15b
X3ne <X3ne@users.noreply.github.com>
2024-04-25 09:32:20 +0200

    feat: move setup.py

--- gen/src/python/setup.py
+++ gen/src/python/setup.py
@@ -4,6 +4,7 @@ setup(
     name='grpc-user-management-auth',
     version='0.1.0-pre',
     packages=find_packages(),
+    package_dir={'': 'auth'},
     install_requires=[
         'grpcio',
     ],
