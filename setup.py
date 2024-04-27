2255607111b8c300cec9ebf7e98150f451e73845
X3ne <X3ne@users.noreply.github.com>
2024-04-27 18:00:59 +0200

    feat: switch to betterproto codegen

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='grpc-user-management-auth',
-    version='1.0.1',
-    package_dir={'grpc_user_management_auth': 'gen/src/python/auth'},
+    version='1.0.2',
+    package_dir={'grpc_user_management_auth': 'gen/src/python'},
     install_requires=[
-        'grpcio',
+        'betterproto',
     ],
 )
