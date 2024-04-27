d3b0a40419e6a9ac7ec411c4e91fc8946d9792d6
X3ne <X3ne@users.noreply.github.com>
2024-04-27 14:54:38 +0200

    fix: change python package version

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
-import os
-
-version = os.getenv('VERSION')
 
 setup(
     name='grpc-user-management-auth',
-    version=version,
+    version='1.0.1',
     package_dir={'grpc_user_management_auth': 'gen/src/python/auth'},
     install_requires=[
         'grpcio',
