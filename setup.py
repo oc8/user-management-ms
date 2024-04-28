c1f567e7459ffb8d30b0ac2e30e94794335fba4a
X3ne <X3ne@users.noreply.github.com>
2024-04-28 15:46:38 +0200

    build: change python module to 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auth',
-    version='1.0.2',
+    version='1.0.3',
     package_dir={'': 'gen/src/python'},
     install_requires=[
         'betterproto',
